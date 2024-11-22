bece0bd35d20ba8f16f40f642d55316d06c8bbc7
Bruno Fernando da Silva <bruno.fernando@nttdata.com>
2024-11-22 23:31:50 +0000

    Ajustando exib de data de nascimento

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,74 +1,74 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment"
+    "sap/ui/core/Fragment",
 ],
-function (Controller, JSONModel, Fragment) {
-    "use strict";
+    function (Controller, JSONModel, Fragment) {
+        "use strict";
 
-    return Controller.extend("mentoria.ka.fiori.zpraticamodel1bfs.controller.View1", {
+        return Controller.extend("mentoria.ka.fiori.zpraticamodel1bfs.controller.View1", {
 
-        onInit: function () {
-            
-            var oCadastros = new JSONModel();
-            oCadastros.loadData("/model/Cadastros.json");
-            this.getView().setModel(oCadastros);
-            
-        },
+            onInit: function () {
 
-        onNovoPress: function (oEvent) {
+                var oCadastros = new JSONModel();
+                oCadastros.loadData("/model/Cadastros.json");
+                this.getView().setModel(oCadastros);
 
-            var oView = this.getView(),
-                oDialogNewReg = this.getView().byId("idDialogNewReg");
-            if (!oDialogNewReg) {
-                Fragment.load({
-                    id: oView.getId(),
-                    name: "mentoria.ka.fiori.zpraticamodel1bfs.view.DialogNewRegister",
-                    type: "XML",
-                    controller: this
-                }).then(function (oDialog) {
-                    oView.addDependent(oDialog);
+            },
 
-                    oDialog.bindElement({path: "/dataForm"});
-                    oDialog.open();
-                })
-            } else {
-                oDialogNewReg.open();
-            }
+            onNovoPress: function (oEvent) {
 
-        }, 
+                var oView = this.getView(), oDialogNewReg = this.getView().byId("idDialogNewReg");
+                if (!oDialogNewReg) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "mentoria.ka.fiori.zpraticamodel1bfs.view.DialogNewRegister",
+                        type: "XML",
+                        controller: this
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
 
-        onCancelButton: function () {
-            this.getView().byId("idDialogNewReg").close();
-        },
+                        oDialog.bindElement({ path: "/dataForm" });
+                        oDialog.open();
+                    });
+                } else {
+                    oDialogNewReg.open();
+                }
 
-        onSalveButton: function (oEvent) {
-            let oModelForm = this.getView().getModel().getProperty("/dataForm");
- 
-            let oModelTable = this.getView().getModel().getProperty("/dataTable");
- 
- 
-            let sSexo = this.getView().byId("selectedSexo").getSelectedKey();
- 
-            oModelTable.push(oModelForm);
- 
-            oEvent.getSource().getParent().destroy();
- 
-            this.getView().getModel().setProperty("/dataTable", oModelTable);
- 
- 
-            let oModelFormNew = Object.create(oModelForm);
- 
-            for (const property in oModelFormNew) {
-                oModelFormNew[property] = "";
-            };
- 
-            this.getView().getModel().setProperty("/dataForm", oModelFormNew);
+            },
 
-            this.byId("idPanel").getBinding("items").refresh();
+            onCancelButton: function () {
+                this.getView().byId("idDialogNewReg").close();
+            },
 
-            this.getView().byId("idDialogNewReg").close();
-        },
+            onSalveButton: function (oEvent) {
+                let oModelForm = this.getView().getModel().getProperty("/dataForm");
 
+                let oModelTable = this.getView().getModel().getProperty("/dataTable");
+
+
+                let sSexo = this.getView().byId("selectedSexo").getSelectedKey();
+
+                oModelTable.push(oModelForm);
+
+                oEvent.getSource().getParent().destroy();
+
+                this.getView().getModel().setProperty("/dataTable", oModelTable);
+
+
+                let oModelFormNew = Object.create(oModelForm);
+
+                for (const property in oModelFormNew) {
+                    oModelFormNew[property] = "";
+                };
+
+                this.getView().getModel().setProperty("/dataForm", oModelFormNew);
+
+                this.byId("idPanel").getBinding("items").refresh();
+
+                this.getView().byId("idDialogNewReg").close();
+            },
+        });
     });
-});
+
